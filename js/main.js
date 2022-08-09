067fb1ca9a0e274957cc73c474ba6201279cb685
Mazya <almazbekbekeev.@gmail.com>
2022-08-09 21:42:44 +0600

    add js module

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { conversionF } from './app.js';
+conversionF();
+
+import { renderKSGRate } from './app.js';
+renderKSGRate();
+
+import { calcDate } from './orozo.js';
+window.calcDate = calcDate;
+
